4b94bf42a2994c4702ac77cf9a7f65cc3737ac95
DOOMxSLAYER <84665718+DOOMxSLAYER@users.noreply.github.com>
2021-06-19 02:41:29 +0530

    Update create.sql

--- create.sql
+++ create.sql
@@ -1,19 +1,19 @@
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE publisher(
-	id integer primary key,
+	id integer primary key autoincrement,
 	name text,
 	country text
 );
 
 CREATE TABLE books(
-	id integer primary key,
+	id integer primary key autoincrement,
 	title text,
 	publisher integer references publisher(id)
 );
 
 CREATE TABLE subjects(
-	id integer primary key,
+	id integer primary key autoincrement,
 	name text
 );
 

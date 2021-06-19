4bd86435a1e6ba2dba0eef2f1108dbcccb0d64e8
DOOMxSLAYER <84665718+DOOMxSLAYER@users.noreply.github.com>
2021-06-19 23:31:32 +0530

    Update query4.sql

--- query4.sql
+++ query4.sql
@@ -1,1 +1,1 @@
-select subject from books_subjects where book="Atomic Habits";
+select s.name from books b, subjects s, books_subjects p where b.title="Atomic Habits" and b.id=p.book and p.subject=s.id;

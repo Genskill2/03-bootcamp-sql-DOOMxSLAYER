817eeb5dff8cee33c4aac9978a1366bc2494a7b8
DOOMxSLAYER <84665718+DOOMxSLAYER@users.noreply.github.com>
2021-06-19 23:40:26 +0530

    Update delete1.sql

--- delete1.sql
+++ delete1.sql
@@ -1,2 +1,2 @@
-delete from books where exists ( select * from books b, subjects s, books_publishers p where s.name="History" and s.id=p.subjects);
+delete from books where exists ( select b.title from books b, subjects s, books_publishers p where s.name="History" and s.id=p.subjects and b.id=p.book);
 delete from subjects where name="History";

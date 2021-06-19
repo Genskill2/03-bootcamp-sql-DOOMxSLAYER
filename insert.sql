aefdd0c3e3ea51fd73ee68e61a9e73f512455bf9
DOOMxSLAYER <84665718+DOOMxSLAYER@users.noreply.github.com>
2021-06-19 14:52:59 +0530

    Update insert.sql

--- insert.sql
+++ insert.sql
@@ -5,33 +5,33 @@ insert into publisher(name, country) values ("Avery", "USA");
 insert into publisher(name, country) values ("Del Rey", "UK");
 insert into publisher(name, country) values ("Vintage", "UK");
 
-insert into books(title, publisher) values ("The C Programming Language", "PHI");
-insert into books(title, publisher) values ("The Go Programming Language", "PHI");
-insert into books(title, publisher) values ("The UNIX Programming Environment", "PHI");
-insert into books(title, publisher) values ("Cryptonomicon", "Harper");
-insert into books(title, publisher) values ("Deep Work", "GCP");
-insert into books(title, publisher) values ("Atomic Habits", "Avery");
-insert into books(title, publisher) values ("The City and The City", "Del Rey");
-insert into books(title, publisher) values ("The Great War for Civilisation", "Vintage");
+insert into books(title, publisher) values ("The C Programming Language", 1);
+insert into books(title, publisher) values ("The Go Programming Language", 1);
+insert into books(title, publisher) values ("The UNIX Programming Environment", 1);
+insert into books(title, publisher) values ("Cryptonomicon", 2);
+insert into books(title, publisher) values ("Deep Work", 3);
+insert into books(title, publisher) values ("Atomic Habits", 4);
+insert into books(title, publisher) values ("The City and The City", 5);
+insert into books(title, publisher) values ("The Great War for Civilisation", 6);
 
 insert into subjects(name) values ("C"), ("UNIX"), ("Technology"), ("Go"), ("Science Fiction"), ("Productivity"), ("Psychology"), ("Politics"), ("History");
 
 insert into books_subjects(book, subject)
 values
-	("The C Programming Language", "C"),
-	("The C Programming Language", "UNIX"),
-	("The C Programming Language", "Technology"),
-	("The Go Programming Language", "Go"),
-	("The Go Programming Language", "Technology"),
-	("The UNIX Programming Environment", "UNIX"),
-	("The UNIX Programming Environment", "Technology"),
-	("Cryptonomicon", "Technology"),
-	("Cryptonomicon", "Science Fiction"),
-	("Deep Work", "Technology"),
-	("Deep Work", "Productivity"),
-	("Atomic Habits", "Science Fiction"),
-	("Atomic Habits", "Psychology"),
-	("The City and The City", "Science Fiction"),
-	("The City and The City", "Politics"),
-	("The Great War for Civilisation", "Politics"),
-	("The Great War for Civilisation", "History");
+	(1, 1),
+	(1, 2),
+	(1, 3),
+	(2, 4),
+	(2, 3),
+	(3, 2),
+	(3, 3),
+	(4, 3),
+	(4, 5),
+	(5, 3),
+	(5, 6),
+	(6, 6),
+	(6, 7),
+	(7, 5),
+	(7, 8),
+	(8, 8),
+	(8, 9);

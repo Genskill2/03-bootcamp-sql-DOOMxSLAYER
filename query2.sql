976b006745aaab09a0330e8bed63fa27bf45cc0d
DOOMxSLAYER <84665718+DOOMxSLAYER@users.noreply.github.com>
2021-06-19 23:20:13 +0530

    Update query2.sql

--- query2.sql
+++ query2.sql
@@ -1,1 +1,1 @@
-select b.title, p.name from books b, publisher p where p.country="uk" and p.id=b.publisher;
+select b.title, p.name from books b, publisher p where p.country="UK" and p.id=b.publisher;
